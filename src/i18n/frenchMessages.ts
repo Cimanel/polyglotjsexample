550827048579713cee7029d0330a77a739afdb9c
Cimanel <cindy@marmelab.com>
2022-09-23 14:49:32 +0200

    add polyglot

---
+++ src/i18n/frenchMessages.ts
@@ -0,0 +1,31 @@
+const frenchMessages = {
+  users: {
+    name: 'Utilisateur |||| Utilisateurs',
+    title: 'Bonjour %{name}',
+    fields: {
+      id: 'ID',
+      identity: {
+        title: 'Identité',
+        name: 'Nom',
+        username: 'Prénom',
+        email: 'Email',
+        phone: 'Numéro de téléphone',
+        website: 'Site web',
+      },
+
+      address: {
+        title: 'Addresse',
+        street: 'Rue',
+        city: 'Ville',
+        zipcode: 'Code postal',
+      },
+      company: {
+        title: 'Entreprise',
+        name: 'Nom',
+        catchPhrase: "Phrase d'accroche",
+      },
+    },
+  },
+};
+
+export default frenchMessages;
